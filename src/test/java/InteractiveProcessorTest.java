2b80294dada58c740528c08d13f74cfa32aa2dbe
FuncGuy <tim25659@gmail.com>
2020-01-26 07:40:09 +0530

    correct the indentation.

--- src/test/java/InteractiveProcessorTest.java
+++ src/test/java/InteractiveProcessorTest.java
@@ -6,21 +6,21 @@ import static org.junit.Assert.fail;
 
 public class InteractiveProcessorTest {
 
-	static InteractiveParkingLotProcessor processor = null;
+    static InteractiveParkingLotProcessor processor = null;
 
-	@BeforeClass
-	public static void setUp() throws Exception {
-		processor = new InteractiveParkingLotProcessor();
-	}
+    @BeforeClass
+    public static void setUp() throws Exception {
+        processor = new InteractiveParkingLotProcessor();
+    }
 
-	@Test
-	public void should_create_six_parking_slots() {
-		try {
-			processor.validateAndProcess("create_parking_lot 6");
-		} catch (Exception e) {
-			fail("Creation of parking slot failed");
-		}
-	}
+    @Test
+    public void should_create_six_parking_slots() {
+        try {
+            processor.validateAndProcess("create_parking_lot 6");
+        } catch (Exception e) {
+            fail("Creation of parking slot failed");
+        }
+    }
 
     @Test
     public void should_display_error_message_when_command_is_invalid() {
